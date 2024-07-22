88b22960339e13f80f414676c392c985058b0369
antonfoch <anton.foch@epitech.digital>
2024-07-22 02:19:55 +0200

    Fixed app

--- Site/rugby-predictor/src/App.js
+++ Site/rugby-predictor/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Loading from 'react-loading';
+import Confetti from 'react-confetti';
 import './App.css';
 
 class RugbyPredictor extends Component {
@@ -9,9 +11,10 @@ class RugbyPredictor extends Component {
       homeTeam: '',
       awayTeam: '',
       prediction: '',
+      loading: false,
+      confettiActive: false,
     };
 
-    // Tableau des équipes
     this.teams = ['Argentina', 'Australia', 'England', 'France', 'Ireland', 'Italy', 'New Zealand', 'Scotland', 'South Africa', 'Wales'];
   }
 
@@ -25,54 +28,61 @@ class RugbyPredictor extends Component {
 
   handlePredictClick = () => {
     const { homeTeam, awayTeam } = this.state;
+    this.setState({ loading: true });
 
-    // Effectuez une requête HTTP POST vers l'API Flask
-    axios.post('http://127.0.0.1:5000/predict', { home_team: homeTeam, away_team: awayTeam })
-      .then((response) => {
-        this.setState({ prediction: response.data.winning_team });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    setTimeout(() => {
+      axios.post('http://127.0.0.1:5000/predict', { home_team: homeTeam, away_team: awayTeam })
+        .then((response) => {
+          this.setState({ prediction: response.data.winning_team, loading: false });
+        })
+        .catch((error) => {
+          console.error(error);
+          this.setState({ loading: false }); // Arrêt du chargement en cas d'erreur
+        });
+    }, 4000);
   }
 
   render() {
     return (
       <div className="RugbyPredictor">
         <h1 className='RugbyTitle'>Rugby Match Predictor</h1>
-        <div className='Container'>
-          <div className='OptionContainer'>
-            <div className='Home'>
-              <label>Home</label>
-              <select onChange={this.handleHomeTeamChange}>
-                <option value="">Select a team</option>
-                {this.teams.map((team, index) => (
-                  <option key={index} value={team}>{team}</option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <p>VS</p>
-            </div>
-            <div className='Away'>
-              <label>Away</label>
-              <select onChange={this.handleAwayTeamChange}>
-                <option value="">Select a team</option>
-                {this.teams.map((team, index) => (
-                  <option key={index} value={team}>{team}</option>
-                ))}
-              </select>
-            </div>
+        <div className='OptionContainer'>
+          <div className='Home'>
+            <label>Home</label>
+            <select onChange={this.handleHomeTeamChange}>
+              <option value="">Select a team</option>
+              {this.teams.map((team, index) => (
+                <option key={index} value={team}>{team}</option>
+              ))}
+            </select>
           </div>
-          <button onClick={this.handlePredictClick}>Predict Result</button>
-          {this.state.prediction && (
-            <div>Result of the prediction: {this.state.prediction}</div>
+          <div className='middle'>
+            <button onClick={this.handlePredictClick} className='predictbutton'>Predict Result</button>
+          </div>
+          <div className='Away'>
+            <label>Away</label>
+            <select onChange={this.handleAwayTeamChange}>
+              <option value="">Select a team</option>
+              {this.teams.map((team, index) => (
+                <option key={index} value={team}>{team}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className='resultcontainer'>
+          {this.state.loading ? (
+            <Loading type="spinningBubbles" color="#007bff" height={50} width={50} />
+          ) : (
+            this.state.prediction && (
+              <div>
+                <Confetti numberOfPieces={200} />
+                <p className='result'>Winner: {this.state.prediction}</p>
+              </div>
+            )
           )}
         </div>
-
-
-
       </div>
+
     );
   }
 }
--- algorithme.py
+++ algorithme.py
@@ -4,35 +4,27 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
-# Étape 1 : Charger les données à partir du fichier CSV
 data = pd.read_csv("donneesrugby.csv")
 
-# Étape 2 : Prétraiter les données
-# Utiliser Label Encoding pour convertir les noms d'équipes en identifiants numériques
 le = LabelEncoder()
 data['home_team_id'] = le.fit_transform(data['home_team'])
 data['away_team_id'] = le.transform(data['away_team'])
 
-# Sélectionner les caractéristiques (features) et la cible (target)
 X = data[['home_team_id', 'away_team_id']]
 y = (data['home_score'] > data['away_score']).astype(
-    int)  # 1 si l'équipe à domicile gagne, 0 sinon
+    int)
 
-# Étape 3 : Entraîner un modèle de classification (Random Forest)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X, y)
 
 joblib.dump(model, 'rugby_model2.joblib')
 
-# Étape 4 : Faire des prédictions pour de nouveaux matchs depuis l'entrée standard (terminal)
 home_team = input("Nom de l'équipe à domicile : ")
 away_team = input("Nom de l'équipe à l'extérieur : ")
 
-# Utiliser LabelEncoder pour convertir les noms des équipes en identifiants
 home_team_id = le.transform([home_team])[0]
 away_team_id = le.transform([away_team])[0]
 
-# Créer une nouvelle entrée pour la prédiction
 new_match = [[home_team_id, away_team_id]]
 prediction = model.predict(new_match)
 
